4df968597e61da90fe8c58524a9364528d209aeb
dorisy <948270260@qq.com>
2017-04-24 11:52:03 +0800

    server change

--- websocket/client.js
+++ websocket/client.js
@@ -97,7 +97,7 @@
 			this.scrollToBottom();
 			
 			//连接websocket后端服务器
-			this.socket = io.connect('ws://m.fat22.qa.nt.ctripcorp.com:3000');
+			this.socket = io.connect('ws://m.fat2.qa.nt.ctripcorp.com:3000');
 			
 			//告诉服务器端有用户登录
 			this.socket.emit('login', {userid:this.userid, username:this.username});
